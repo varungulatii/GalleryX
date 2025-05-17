ec1e5347016ac2029353a6c5760003687e78226c
varun gulati <varun.gulati5@gmail.com>
2025-05-18 00:41:58 +0530

    feat(core): add PermissionHelper for API 24-34 media access handling

---
+++ core/src/main/java/com/varun/galleryx/core/permission/PermissionHelper.kt
@@ -0,0 +1,24 @@
+package com.varun.galleryx.core.permission
+
+import android.Manifest
+import android.os.Build
+
+object PermissionHelper {
+
+    private fun requiredPermissions(): Array<String> {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            arrayOf(
+                Manifest.permission.READ_MEDIA_IMAGES,
+                Manifest.permission.READ_MEDIA_VIDEO
+            )
+        } else {
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+    }
+
+    fun allPermissionsGranted(
+        grantedMap: Map<String, Boolean>
+    ): Boolean {
+        return requiredPermissions().all { grantedMap[it] == true }
+    }
+}
