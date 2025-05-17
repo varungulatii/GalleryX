28b6d1c5a6879faf2470e294037c3ce62166d708
varun gulati <varun.gulati5@gmail.com>
2025-05-17 17:14:22 +0530

    chore(hilt): set up Hilt for dependency injection in app and feature modules

---
+++ app/src/main/java/com/varun/galleryx/GalleryXApplication.kt
@@ -0,0 +1,7 @@
+package com.varun.galleryx
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GalleryXApplication : Application()
--- app/src/main/java/com/varun/galleryx/MainActivity.kt
+++ app/src/main/java/com/varun/galleryx/MainActivity.kt
@@ -6,18 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.varun.galleryx.ui.theme.GalleryXTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
