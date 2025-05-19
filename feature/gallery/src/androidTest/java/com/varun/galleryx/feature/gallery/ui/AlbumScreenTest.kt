a41756de8e133136e5387f55abc267a067d40abd
varun gulati <varun.gulati5@gmail.com>
2025-05-19 10:09:41 +0530

    test(ui): add grid/list toggle layout test for AlbumScreen
    This confirms that layout switching works as expected and UI recomposes correctly.

--- feature/gallery/src/androidTest/java/com/varun/galleryx/feature/gallery/ui/AlbumScreenTest.kt
+++ feature/gallery/src/androidTest/java/com/varun/galleryx/feature/gallery/ui/AlbumScreenTest.kt
@@ -2,7 +2,9 @@ package com.varun.galleryx.feature.gallery.ui
 
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import com.varun.galleryx.domain.model.Album
 import org.junit.Rule
 import org.junit.Test
@@ -26,9 +28,29 @@ class AlbumScreenTest {
             )
         }
 
-        // Assert both albums are visible
-        composeTestRule.onNodeWithTag("albumCard_Camera").assertExists()
-        composeTestRule.onNodeWithTag("albumCard_Screenshots").assertExists()
+        composeTestRule.onNodeWithTag("albumCard_Camera").assertExists() // Camera card should be visible
+        composeTestRule.onNodeWithTag("albumCard_Screenshots").assertExists() // Screenshots card should be visible
+    }
+
+    @Test
+    fun layoutToggles_betweenGridAndList() {
+        val dummyAlbums = listOf(
+            Album(name = "Camera", itemCount = 12, thumbnailUri = "file://img1.jpg"),
+            Album(name = "Screenshots", itemCount = 5, thumbnailUri = "file://img2.jpg")
+        )
+
+        composeTestRule.setContent {
+            AlbumScreenFake(
+                uiState = GalleryUiState.Success(dummyAlbums),
+                onAlbumClick = {}
+            )
+        }
+
+        composeTestRule.onNodeWithTag("albumGrid").assertExists() // Grid should be visible
+        composeTestRule.onNodeWithTag("albumList").assertDoesNotExist() // List should not be visible
+        composeTestRule.onNodeWithContentDescription("Toggle layout").performClick() // Toggle to list
+        composeTestRule.onNodeWithTag("albumList").assertExists() // List should be visible
+        composeTestRule.onNodeWithTag("albumGrid").assertDoesNotExist() // Grid should not be visible
     }
 }
 
