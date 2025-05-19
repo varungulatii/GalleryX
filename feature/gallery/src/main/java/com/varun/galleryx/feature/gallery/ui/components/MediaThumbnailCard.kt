d53ba09b5e43b461dd3966d0404c621a5056d5ec
varun gulati <varun.gulati5@gmail.com>
2025-05-20 00:56:55 +0530

    feat(ui): add video frame extraction in MediaThumbnailCard using MediaMetadataRetriever

--- feature/gallery/src/main/java/com/varun/galleryx/feature/gallery/ui/components/MediaThumbnailCard.kt
+++ feature/gallery/src/main/java/com/varun/galleryx/feature/gallery/ui/components/MediaThumbnailCard.kt
@@ -1,5 +1,8 @@
 package com.varun.galleryx.feature.gallery.ui.components
 
+import android.graphics.Bitmap
+import android.media.MediaMetadataRetriever
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
@@ -12,10 +15,16 @@ import androidx.compose.material.icons.filled.PlayCircle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -24,6 +33,8 @@ import coil.request.ImageRequest
 import com.varun.galleryx.domain.model.MediaItem
 import com.varun.galleryx.domain.model.MediaType
 import com.varun.galleryx.feature.gallery.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @Composable
 fun MediaThumbnailCard(item: MediaItem) {
@@ -31,7 +42,7 @@ fun MediaThumbnailCard(item: MediaItem) {
         AsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
                 .data(item.uri)
-                .size(500)
+                .size(400)
                 .crossfade(true)
                 .placeholder(R.drawable.placeholder_album)
                 .error(R.drawable.placeholder_album)
@@ -41,26 +52,69 @@ fun MediaThumbnailCard(item: MediaItem) {
             modifier = Modifier
                 .fillMaxWidth()
                 .aspectRatio(1f)
-                .padding(2.dp)
+                .padding(4.dp)
                 .clip(RoundedCornerShape(12.dp))
                 .background(MaterialTheme.colorScheme.surfaceVariant)
         )
     } else {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(1f)
-                .clip(RoundedCornerShape(22.dp))
-                .padding(2.dp)
-                .background(Color.Cyan),
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(
-                imageVector = Icons.Default.PlayCircle,
-                contentDescription = "Video thumbnail",
-                modifier = Modifier.size(48.dp),
-                tint = MaterialTheme.colorScheme.onSurfaceVariant
-            )
+
+        var bitmap by remember { mutableStateOf<Bitmap?>(null) }
+        LaunchedEffect(item.uri) {
+            withContext(Dispatchers.IO) {
+                try {
+                    val retriever = MediaMetadataRetriever()
+                    retriever.setDataSource(item.uri.toString())
+                    bitmap = retriever.getFrameAtTime(1_000_000)
+                    retriever.release()
+                } catch (e: Exception) {
+                    bitmap = null
+                }
+            }
+        }
+
+        if(bitmap != null){
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f)
+                    .clip(RoundedCornerShape(22.dp))
+                    .padding(4.dp)
+                    .background(Color.Cyan),
+                contentAlignment = Alignment.Center
+            ) {
+                bitmap?.let {
+                    Image(
+                        bitmap = it.asImageBitmap(),
+                        contentDescription = item.displayName,
+                        modifier = Modifier.matchParentSize(),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+                Icon(
+                    imageVector = Icons.Default.PlayCircle,
+                    contentDescription = "Video thumbnail",
+                    modifier = Modifier.size(48.dp),
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+        }
+        else {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f)
+                    .clip(RoundedCornerShape(22.dp))
+                    .padding(4.dp)
+                    .background(Color.Cyan),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.PlayCircle,
+                    contentDescription = "Video thumbnail",
+                    modifier = Modifier.size(48.dp),
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
         }
     }
 }
